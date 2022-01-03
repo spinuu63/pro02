14f83ac4c12535298ccc08dc5f15a36731e77927
Spinu Gheorghe <gheorghespinu2001@gmail.com>
2022-01-03 12:23:14 +0200

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { appRoutingModule, routingComponents } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from "@angular/material/button";
+import { AccountComponent } from './account/account.component';
+import {MatCheckboxModule} from "@angular/material/checkbox";
+import {MatInputModule} from "@angular/material/input";
+import {MatGridListModule} from "@angular/material/grid-list";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductAlertsComponent,
+    routingComponents,
+    AccountComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    appRoutingModule,
+    MatCheckboxModule,
+    MatInputModule,
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
